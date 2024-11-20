3781d21f6a10feab7361502a3135e254b6e6c332
Bryan <bryanagan25@gmail.com>
2024-11-20 20:04:15 +0800

    Add sqlite3 for user login feature

--- main/driver.py
+++ main/driver.py
@@ -1,11 +1,31 @@
 from main.login import login_page
 from PyQt6.QtWidgets import QApplication
+import sqlite3
+import os
+import pathlib
 
 
 def main():
+    connection = sqlite3.connect('database\\accounts.db')
+    cursor = connection.cursor()
+
+    cursor.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name='accounts'""")
+
+    tableExists = cursor.fetchall()
+
+    if not tableExists:
+        cursor.execute("""CREATE TABLE accounts (
+            username text,
+            password text
+        )""")
+
+    # cursor.execute("INSERT INTO accounts VALUES ('xbryan25','123')")
+
     app = QApplication([])
     login = login_page.LoginPage()
 
+    connection.commit()
+    connection.close()
 
     login.show()
     app.exec()
--- main/login/login_page.py
+++ main/login/login_page.py
@@ -1,6 +1,7 @@
 from main.login.login_page_design import Ui_MainWindow as LoginPageUI
 from main.login.signup_dialog import SignupDialog
 from PyQt6.QtWidgets import QMainWindow
+import sqlite3
 
 
 class LoginPage(QMainWindow, LoginPageUI):
@@ -10,8 +11,37 @@ class LoginPage(QMainWindow, LoginPageUI):
         self.setupUi(self)
 
         self.sign_up_button.clicked.connect(self.signup_clicked)
+        self.login_button.clicked.connect(self.check_if_account_exists)
 
     def signup_clicked(self):
         signup_dialog = SignupDialog()
 
         signup_dialog.exec()
+
+    def check_if_account_exists(self):
+        connection = sqlite3.connect('database\\accounts.db')
+        cursor = connection.cursor()
+
+        all_accounts_username_password = cursor.execute("SELECT username, password FROM accounts").fetchall()
+
+        # Convert each tuple to string
+        all_username = [str(account[0]) for account in all_accounts_username_password]
+        all_password = [str(account[1]) for account in all_accounts_username_password]
+
+        if self.username_lineedit.text() in all_username:
+            print("Pass test 1")
+
+            password_index = all_username.index(self.username_lineedit.text())
+
+            if self.password_lineedit.text() == all_password[password_index]:
+                print("Pass test 2")
+            else:
+                print("Wrong password")
+
+        else:
+            print("Account doesn't exist")
+
+
+        connection.commit()
+        connection.close()
+
