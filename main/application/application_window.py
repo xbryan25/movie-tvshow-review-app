515db7186f5aed29736b4e22d0d25f8fbae1f014
Bryan <bryanagan25@gmail.com>
2025-03-04 20:26:27 +0800

    Transfer login methods to login_page_controls.py

--- main/application/application_window.py
+++ main/application/application_window.py
@@ -5,6 +5,7 @@ from login.login_status_dialog import LoginStatusDialog
 
 from application.application_window_design import Ui_MainWindow as ApplicationWindowUI
 from application.choose_titles_page_controls import ChooseTitlesPageControls
+from application.login_page_controls import LoginPageControls
 
 from utils.user_input_validators import UserInputValidators
 
@@ -17,7 +18,17 @@ class ApplicationWindow(QMainWindow, ApplicationWindowUI):
 
         self.setupUi(self)
 
-        self.add_signals()
+        self.load_controls()
+
+        self.change_to_login_page()
+
+    def load_controls(self):
+        self.login_page_controls = LoginPageControls([self.sign_up_button,
+                                                      self.login_button,
+                                                      self.show_password_checkbox,
+                                                      self.username_lineedit,
+                                                      self.password_lineedit],
+                                                     self)
 
         self.choose_titles_page_controls = ChooseTitlesPageControls([self.search_title_line_edit,
                                                                      self.liked_button,
@@ -30,56 +41,8 @@ class ApplicationWindow(QMainWindow, ApplicationWindowUI):
                                                                      self.gridLayout_2],
                                                                     self)
 
-    def add_signals(self):
-        self.sign_up_button.clicked.connect(self.signup_clicked)
-        self.login_button.clicked.connect(self.check_if_account_exists)
-        self.show_password_checkbox.stateChanged.connect(self.show_password_text_login)
-
-        self.username_lineedit.returnPressed.connect(self.check_if_account_exists)
-        self.password_lineedit.returnPressed.connect(self.check_if_account_exists)
-
-    def show_password_text_login(self):
-        if self.show_password_checkbox.isChecked():
-            self.password_lineedit.setEchoMode(QLineEdit.EchoMode.Normal)
-        else:
-            self.password_lineedit.setEchoMode(QLineEdit.EchoMode.Password)
-
-    def signup_clicked(self):
-        self.signup_dialog = SignupDialog()
-
-        self.signup_dialog.exec()
-
-    # TODO: Rename this function
-    def check_if_account_exists(self):
-        connection = sqlite3.connect('..\\database\\accounts.db')
-        cursor = connection.cursor()
-
-        all_accounts_username_password = cursor.execute("SELECT username, password FROM accounts").fetchall()
-
-        # Convert each tuple to string
-        all_usernames = [str(account[0]) for account in all_accounts_username_password]
-        all_passwords = [str(account[1]) for account in all_accounts_username_password]
-
-        issue_message = UserInputValidators.login_input_validator(self.username_lineedit.text(),
-                                                                  self.password_lineedit.text(),
-                                                                  all_usernames,
-                                                                  all_passwords)
-
-        if issue_message == "":
-            username = self.username_lineedit.text()
-            account_id = cursor.execute("SELECT account_id FROM accounts WHERE username=(:username)",
-                                        {"username": username}).fetchone()[0]
-
-            self.change_to_choose_title_page(account_id)
-
-        else:
-            login_failure_dialog = LoginStatusDialog()
-            login_failure_dialog.text_label.setText(issue_message)
-            login_failure_dialog.setWindowTitle("Login failure.")
-            login_failure_dialog.exec()
-
-        connection.commit()
-        connection.close()
+    def change_to_login_page(self):
+        self.stackedWidget.setCurrentWidget(self.login_page)
 
     def change_to_choose_title_page(self, account_id):
         login_successful_dialog = LoginStatusDialog()
---
+++ main/application/login_page_controls.py
@@ -0,0 +1,77 @@
+from PyQt6.QtWidgets import QMainWindow, QLineEdit
+
+from login.signup_dialog import SignupDialog
+from login.login_status_dialog import LoginStatusDialog
+
+from utils.user_input_validators import UserInputValidators
+
+import sqlite3
+
+class LoginPageControls:
+    def __init__(self, widgets, application_window):
+
+        self.widgets = widgets
+        self.application_window = application_window
+
+        self.account_id = None
+
+        self.load_widgets()
+        self.add_signals()
+
+    def load_widgets(self):
+        self.sign_up_button = self.widgets[0]
+        self.login_button = self.widgets[1]
+        self.show_password_checkbox = self.widgets[2]
+        self.username_lineedit = self.widgets[3]
+        self.password_lineedit = self.widgets[4]
+
+    def add_signals(self):
+        self.sign_up_button.clicked.connect(self.signup_clicked)
+        self.login_button.clicked.connect(self.check_if_account_exists)
+        self.show_password_checkbox.stateChanged.connect(self.show_password_text_login)
+
+        self.username_lineedit.returnPressed.connect(self.check_if_account_exists)
+        self.username_lineedit.returnPressed.connect(self.check_if_account_exists)
+
+    def show_password_text_login(self):
+        if self.show_password_checkbox.isChecked():
+            self.password_lineedit.setEchoMode(QLineEdit.EchoMode.Normal)
+        else:
+            self.password_lineedit.setEchoMode(QLineEdit.EchoMode.Password)
+
+    def signup_clicked(self):
+        self.signup_dialog = SignupDialog()
+
+        self.signup_dialog.exec()
+
+    # TODO: Rename this function
+    def check_if_account_exists(self):
+        connection = sqlite3.connect('..\\database\\accounts.db')
+        cursor = connection.cursor()
+
+        all_accounts_username_password = cursor.execute("SELECT username, password FROM accounts").fetchall()
+
+        # Convert each tuple to string
+        all_usernames = [str(account[0]) for account in all_accounts_username_password]
+        all_passwords = [str(account[1]) for account in all_accounts_username_password]
+
+        issue_message = UserInputValidators.login_input_validator(self.username_lineedit.text(),
+                                                                  self.password_lineedit.text(),
+                                                                  all_usernames,
+                                                                  all_passwords)
+
+        if issue_message == "":
+            username = self.username_lineedit.text()
+            account_id = cursor.execute("SELECT account_id FROM accounts WHERE username=(:username)",
+                                        {"username": username}).fetchone()[0]
+
+            self.application_window.change_to_choose_title_page(account_id)
+
+        else:
+            login_failure_dialog = LoginStatusDialog()
+            login_failure_dialog.text_label.setText(issue_message)
+            login_failure_dialog.setWindowTitle("Login failure.")
+            login_failure_dialog.exec()
+
+        connection.commit()
+        connection.close()
